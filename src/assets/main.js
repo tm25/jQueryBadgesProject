a05a72456ad433886620b7e778e82e5e466a76af
tm25 <tm25@hawaii.edu>
2017-03-02 12:03:05 -1000

    final

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,40 @@
 $(function() {
 
-  // your code will go here0
-  console.log("Making sure github works");
+ $.ajax({
+    url: 'https://www.codeschool.com/users/taylormaushardt.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourses(response.courses.completed);
+    }
+  });
+
+function addCourses(courses) {
+
+  var $badges = $('#badges');
+
+   courses.forEach(function(course) {
+
+    var $course = $('<div />', {
+       'class': 'course'
+     }).appendTo($badges);
+
+     $('<h3 />', {
+       text: course.title
+     }).appendTo($course);
+
+     $('<img />', {
+       src: course.badge
+     }).appendTo($course);
+
+     $('<a />',{
+       'class': 'btn btn-primary',
+       target: '_blank',
+       href: course.url,
+       text: 'See Course'
+     }).appendTo($course);
+
+   })
+
+ }
+
 });
